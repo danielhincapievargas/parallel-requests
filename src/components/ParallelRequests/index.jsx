4c9cf5fda83e868474a08244d1e35d2b4c7c615b
danielhincapievargas <daniel.hincapie.vargas@gmail.com>
2023-07-23 18:58:26 -0500

    Parallel Request Done

--- src/components/ParallelRequests/index.jsx
+++ src/components/ParallelRequests/index.jsx
@@ -1,58 +1,54 @@
 import './ParallelRequests.scss'
 import {useState, useEffect} from 'react';
-import { getEpisodesWithCharacters } from './utils';
 
-const fetchEpisodes = async () => {
+
+const getAllEpisodes = async () => {
   const response = await fetch('https://rickandmortyapi.com/api/episode');
-  return response.json(); 
+  const data = await response.json();
+  return data.results; 
 }
 
-const fetchCharactersList = async () => {
-  const response = await fetch('https://rickandmortyapi.com/api/character');
-  return response.json(); 
+const getCharacterInfo = async (url) => {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data; 
+}
+
+const getData = async () => {
+  const episodes = await getAllEpisodes();
+
+  const characters = episodes.reduce((acc, item) => {
+    return [...acc, ...item.characters.slice(0, 10)]
+  }, [])
+
+  const characterPromise = characters.map((url) => {
+    return getCharacterInfo(url);
+  });
+  
+  const result = await Promise.all(characterPromise);
+
+  const data = episodes.map((episode) => {
+    return {
+      id: episode.id,
+      title: `${episode.name} - ${episode.episode}`,
+      airDate: episode.air_date,
+      characters: episode.characters.slice(0, 10).map((url) => {
+      return result.find((item) => item.url === url)
+      })
+    }
+  })
+  return data
 }
 
 const ParallelRequest = () => {
-
-  const [episodesAndCharacters, setEpisodesAndCharacters] = useState([
-    {     
-        id: "",
-        name: "",
-        air_date: "",
-        episode: "",
-        characters: {},
-        url: "",
-        created: "",
-        charactersList: [
-          {
-            id: "",
-            name: "",
-            status: "",
-            species: "",
-            type: "",
-            gender: "",
-            origin: {},
-            location: {},
-            image: "",
-            episode: [],
-            url: "",
-            created: ""
-          }]  
-    }
-  ])
+  const [data, setData] = useState([])
 
   useEffect(() => {
-    const fetchAll = async () => {
-      const episodesPromise = fetchEpisodes()
-      const charactersListPromise = fetchCharactersList()
-
-      const [episodesData, charactersListData] = await Promise.all([episodesPromise, charactersListPromise])
-      
-      const data = getEpisodesWithCharacters(episodesData.results, charactersListData.results);
-      setEpisodesAndCharacters(data);
-    }
-    fetchAll()
+    getData().then((data) => {
+      setData(data)
+    })
   }, [])
+  
 
   return (
     <>
@@ -60,13 +56,13 @@ const ParallelRequest = () => {
         <img src="/Rick_and_Morty.svg.png" alt="logo" />
       </div>
       <div className="cards-container">
-        {episodesAndCharacters.map((episode) => (
-          <div className="card" key={episode.id}>
-            <div className="card-title">{`${episode.id}. ${episode.name} - ${episode.episode}`}</div>
-            <div className="card-date">{`Air Date: ${episode.air_date}`}</div>
+        {data.map((item) => (
+          <div className="card" key={item.id}>
+            <div className="card-title">{item.title}</div>
+            <div className="card-date">{`Air Date: ${item.airDate}`}</div>
             <div className="characters-title">Characters</div>
             <div className="characters-container">
-              {episode.charactersList.map((character) => (
+              {item.characters.map((character) => (
                 <div className="character" key={character.id}>
                     {`- ${character.name}  (${character.species})`}
                 </div>
